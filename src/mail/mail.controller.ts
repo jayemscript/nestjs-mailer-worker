d54aae0081b32332f1bd624425dddc4be82386c3
jayemscript <johnmarkpulmano052802@gmail.com>
2026-06-29 11:09:38 +0800

    feat: added api-key-guard

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,11 +1,20 @@
 //src/mail/mail.controller.ts
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { MailService } from './mail.service';
 import { SendEmailDto } from './dto/send-email.dto';
 import { CommonResponseDto } from 'src/common/dtos/common-response.dto';
 import { MESSAGES } from 'src/common/constants/message.constant';
 import { EmailLog } from './schemas/email-log.schema';
+import { ApiKeyGuard } from 'src/common/guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
